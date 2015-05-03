5154e0a0031cdcabb72b800b8eeff83216f8245a
Alain Péteut <alain.peteut@yahoo.com>
2015-05-04 01:01:07 +0200

    use Altera vendor id for now

--- misoclib/com/litepcie/software/linux/kernel/config.h
+++ misoclib/com/litepcie/software/linux/kernel/config.h
@@ -2,7 +2,7 @@
 #define __HW_CONFIG_H
 
 /* pci */
-#define PCI_FPGA_VENDOR_ID 0x10ee
+#define PCI_FPGA_VENDOR_ID 0x1172 // FIXME: add 0x10ee
 #define PCI_FPGA_DEVICE_ID 0x7022
 #define PCI_FPGA_BAR0_SIZE 0xa000
 
